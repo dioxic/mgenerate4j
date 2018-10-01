d1ab375c8faa6f87ebab89756b410b1cc71b9540
Mark Baker-Munton <dioxic@gmail.com>
2018-10-01 10:05:35 +0100

    travis -> java8

--- mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/Main.java
+++ mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/Main.java
@@ -9,7 +9,6 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class Main {
