e5e85cd3cb024126ab1a550f1def71246724c169
Mark Baker-Munton <dioxic@gmail.com>
2019-09-09 21:46:35 +0100

    changed Now operator to use LocalDateTime

--- mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/time/Now.java
+++ mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/time/Now.java
@@ -3,14 +3,15 @@ package uk.dioxic.mgenerate.core.operator.time;
 import uk.dioxic.mgenerate.common.Resolvable;
 import uk.dioxic.mgenerate.common.annotation.Operator;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Operator
-public class Now implements Resolvable<Date> {
+public class Now implements Resolvable<LocalDateTime> {
 
 	@Override
-	public Date resolve() {
-		return new Date();
+	public LocalDateTime resolve() {
+		return LocalDateTime.now();
 	}
 
 }
