f7221d0ba98715ecb74d7d9355cd4c56de91a811
Mark Baker-Munton <dioxic@gmail.com>
2019-09-14 15:57:45 +0100

    missing operator property

--- mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/time/Hour.java
+++ mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/time/Hour.java
@@ -3,11 +3,13 @@ package uk.dioxic.mgenerate.core.operator.time;
 import uk.dioxic.mgenerate.common.Resolvable;
 import uk.dioxic.mgenerate.common.Wrapper;
 import uk.dioxic.mgenerate.common.annotation.Operator;
+import uk.dioxic.mgenerate.common.annotation.OperatorProperty;
 import uk.dioxic.mgenerate.core.util.FakerUtil;
 
 @Operator
 public class Hour implements Resolvable<Integer> {
 
+    @OperatorProperty
     private Resolvable<Boolean> twentyFour = Wrapper.wrap(Boolean.FALSE);
 
     @Override
