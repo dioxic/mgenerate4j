31df3a839007af1353f9b5c9d3055084b465b0ac
Mark Baker-Munton <dioxic@gmail.com>
2020-01-16 15:59:59 +0000

    FIX: some chronounits were not incremented correctly (e.g. months)

--- mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/sequence/DateSequence.java
+++ mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/sequence/DateSequence.java
@@ -9,7 +9,12 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.LongUnaryOperator;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
 
 @Operator({"dateSeq","dateSequence"})
 public class DateSequence extends AbstractOperator<LocalDateTime> implements Initializable {
@@ -24,16 +29,27 @@ public class DateSequence extends AbstractOperator<LocalDateTime> implements Ini
     LocalDateTime start = LocalDateTime.now();
 
     private AtomicLong counter;
+    private LongUnaryOperator inc;
 
     @Override
     public LocalDateTime resolveInternal() {
-        return LocalDateTime.ofInstant(Instant.ofEpochMilli(counter.getAndUpdate(n -> n + step)), ZoneOffset.UTC);
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(counter.getAndUpdate(inc)), ZoneOffset.UTC);
     }
 
     @Override
     public void initialize() {
         counter = new AtomicLong(start.toInstant(ZoneOffset.UTC).toEpochMilli());
-        step = toMillis(chronoUnit, step);
+
+        List<ChronoUnit> simpleInc = Arrays.asList(ChronoUnit.NANOS,MILLIS,ChronoUnit.SECONDS,ChronoUnit.MINUTES,ChronoUnit.HOURS,ChronoUnit.DAYS,ChronoUnit.WEEKS);
+
+        if (simpleInc.contains(chronoUnit)) {
+            step = convertToMillis(chronoUnit, step);
+            chronoUnit = MILLIS;
+            inc = millisInc;
+        }
+        else {
+            inc = dtInc;
+        }
     }
 
     /**
@@ -41,10 +57,23 @@ public class DateSequence extends AbstractOperator<LocalDateTime> implements Ini
      *
      * @return
      */
-    private Long toMillis(ChronoUnit chronoUnit, Long step) {
+    private Long convertToMillis(ChronoUnit chronoUnit, Long step) {
         LocalDateTime dt = LocalDateTime.now();
         LocalDateTime dtStep = dt.plus(step, chronoUnit);
 
         return dtStep.toInstant(ZoneOffset.UTC).toEpochMilli() - dt.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
+
+    private LocalDateTime toLdt(long millis) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneOffset.UTC);
+    }
+
+    private long toMillis(LocalDateTime ldt) {
+        return ldt.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+
+    private LongUnaryOperator dtInc = n -> toMillis(toLdt(n).plus(step, chronoUnit));
+
+    private LongUnaryOperator millisInc = n -> n + step;
+
 }
