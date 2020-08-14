e49069c63647aec080e7876492bdef04f31de65c
Mark Baker-Munton <dioxic@gmail.com>
2020-08-14 20:32:28 +0100

    abstraction of gradle build logic to composite plugin

--- mgenerate-core/build.gradle.kts
+++ mgenerate-core/build.gradle.kts
@@ -1,21 +1,26 @@
-//plugins {
-//    `java-library`
-//}
+import uk.dioxic.build.Deps
+
+plugins {
+    `java-library`
+    id("uk.dioxic.build") version "1.0"
+}
 
 description = "Core code for mgenerate4j"
-extra["displayName"] = "Mgenerate Core"
-extra["moduleName"] = "uk.dioxic.mgenerate.core"
+
+buildConfig {
+    displayName = "Mgenerate Core"
+    moduleName = "uk.dioxic.mgenerate.core"
+    publish = true
+}
 
 dependencies {
     implementation(platform(project(":mgenerate-bom")))
     annotationProcessor(project(":mgenerate-apt"))
     api(project(":mgenerate-common"))
-
-    implementation("org.mongodb:bson")
-
     implementation("info.picocli:picocli:4.1.2")
-    implementation("uk.dioxic.faker4j:faker4j:0.0.4")
-    implementation("ch.rasc:bsoncodec:1.0.1")
-    implementation("commons-codec:commons-codec:1.13")
-    implementation("org.reflections:reflections:0.9.11")
+    implementation(Deps.bson)
+    implementation(Deps.faker4j)
+    implementation(Deps.bsoncodec)
+    implementation(Deps.commonsCodec)
+    implementation(Deps.reflections)
 }
