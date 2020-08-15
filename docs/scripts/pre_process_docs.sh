d9c68ed6ffeb777fb31954e3b50bbb129f60d597
Mark Baker-Munton <dioxic@gmail.com>
2020-08-15 10:39:17 +0100

    FIX to remove new badges from gh-pages home (take 2)

--- docs/scripts/pre_process_docs.sh
+++ docs/scripts/pre_process_docs.sh
@@ -11,7 +11,7 @@ cat CHANGELOG.md \
 # Copy the README into the index, omitting the license, docs links, and fixing hrefs
 cat README.md \
   | sed 's:docs/img:img:g' \
-  | grep -wvE '_badge' \
+  | grep -vE '_badge' \
   | sed -e '/^## Documentation/,/^## /{//!d}' \
   | sed -e '/^## Documentation/d' \
   | sed -e '/^## License/,/^\[hack\]/d' \
