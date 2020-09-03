8be4db872a3972c6d9e2442af895941c67f0064f
Mark Baker-Munton <dioxic@gmail.com>
2020-09-03 13:00:27 +0100

    spek added to dependency list

--- buildPlugin/src/main/kotlin/uk/dioxic/gradle/Dependencies.kt
+++ buildPlugin/src/main/kotlin/uk/dioxic/gradle/Dependencies.kt
@@ -1,17 +1,21 @@
 package uk.dioxic.gradle
 
 object Dependencies {
-    const val junit5 = "org.junit.jupiter:junit-jupiter:${Versions.junit5}"
-    const val slf4jApi = "org.slf4j:slf4j-api:${Versions.slf4j}"
-    const val assertj = "org.assertj:assertj-core:${Versions.assertj}"
+
     const val reactorCore = "io.projectreactor:reactor-core:${Versions.reactor}"
-    const val reactorTest = "io.projectreactor:reactor-test:${Versions.reactor}"
     const val clikt = "com.github.ajalt:clikt:${Versions.clikt}"
     const val faker4j = "uk.dioxic.faker4j:faker4j:${Versions.faker4j}"
     const val reflections = "org.reflections:reflections:${Versions.reflections}"
     const val commonsCodec = "commons-codec:commons-codec:${Versions.commonsCodec}"
     const val bsoncodec = "ch.rasc:bsoncodec:${Versions.bsoncodec}"
     const val javapoet = "com.squareup:javapoet:${Versions.javapoet}"
+
+    // testing
+    const val junit5 = "org.junit.jupiter:junit-jupiter:${Versions.junit5}"
+    const val assertj = "org.assertj:assertj-core:${Versions.assertj}"
+    const val reactorTest = "io.projectreactor:reactor-test:${Versions.reactor}"
+    const val spekDsl = "org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}}"
+    const val spekRunnerJunit5 = "org.spekframework.spek2:spek-runner-junit5:${Versions.spek}"
 
     // plugins
     const val shadowPlugin = "com.github.jengelman.gradle.plugins:shadow:${Versions.shadowPlugin}"
@@ -36,5 +40,6 @@ object Dependencies {
     // log4j
     const val log4jCore = "org.apache.logging.log4j:log4j-core:${Versions.log4j}"
     const val log4jSlf4jImpl = "org.apache.logging.log4j:log4j-slf4j-impl:${Versions.log4j}"
+    const val slf4jApi = "org.slf4j:slf4j-api:${Versions.slf4j}"
 
 }
--- buildPlugin/src/main/kotlin/uk/dioxic/gradle/Versions.kt
+++ buildPlugin/src/main/kotlin/uk/dioxic/gradle/Versions.kt
@@ -1,17 +1,10 @@
 package uk.dioxic.gradle
 
 object Versions {
-    const val kotlin = "1.4.0"
 
-    const val log4j = "2.13.3"
     const val mongo = "4.1.0"
     const val reactor = "3.3.8.RELEASE"
-    const val junit5 = "5.6.2"
-    const val assertj = "3.17.1"
-    const val slf4j = "1.7.30"
-    const val kotlinxCoroutines = "1.3.9"
     const val kotlinStatistics = "1.2.1"
-    const val kotlinSerialization = "1.0.0-RC"
     const val clikt = "2.8.0"
     const val faker4j = "0.0.4"
     const val reflections = "0.9.12"
@@ -19,8 +12,24 @@ object Versions {
     const val bsoncodec = "1.0.1"
     const val javapoet = "1.13.0"
 
+    // kotlin
+    const val kotlinxCoroutines = "1.3.9"
+    const val kotlinSerialization = "1.0.0-RC"
+    const val kotlin = "1.4.0"
+
+    // testing
+    const val spek = "2.0.12"
+    const val junit5 = "5.6.2"
+    const val assertj = "3.17.1"
+
+    // logging
+    const val slf4j = "1.7.30"
+    const val log4j = "2.13.3"
+
+
     // plugins
     const val shadowPlugin = "6.0.0"
     const val dokkaPlugin = kotlin
     const val kotlinPlugin = kotlin
+    const val axionPlugin = "1.11.0"
 }
