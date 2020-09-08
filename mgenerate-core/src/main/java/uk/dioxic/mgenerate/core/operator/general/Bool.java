07d68950e2e76e7580203be19f00b541857a1717
Mark Baker-Munton <dioxic@gmail.com>
2020-09-08 10:27:38 +0100

    add probability to bool operator

--- mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/general/Bool.java
+++ mgenerate-core/src/main/java/uk/dioxic/mgenerate/core/operator/general/Bool.java
@@ -1,15 +1,19 @@
 package uk.dioxic.mgenerate.core.operator.general;
 
 import uk.dioxic.mgenerate.common.annotation.Operator;
+import uk.dioxic.mgenerate.common.annotation.OperatorProperty;
 import uk.dioxic.mgenerate.core.operator.AbstractOperator;
 import uk.dioxic.mgenerate.core.util.FakerUtil;
 
 @Operator({"bool", "boolean"})
 public class Bool extends AbstractOperator<Boolean> {
 
+    @OperatorProperty
+    Double probability = 0.5d;
+
     @Override
     public Boolean resolveInternal() {
-        return FakerUtil.randomBoolean();
+        return FakerUtil.numberBetween(1, (int) (probability * 100)) > 50;
     }
 
 }
