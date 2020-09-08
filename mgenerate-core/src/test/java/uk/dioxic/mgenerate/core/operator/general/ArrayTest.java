b6bccde32ac13a609da98b323c707a40111e0719
Mark Baker-Munton <dioxic@gmail.com>
2020-09-08 16:49:40 +0100

    remove erroneous test

--- mgenerate-core/src/test/java/uk/dioxic/mgenerate/core/operator/general/ArrayTest.java
+++ mgenerate-core/src/test/java/uk/dioxic/mgenerate/core/operator/general/ArrayTest.java
@@ -3,26 +3,11 @@ package uk.dioxic.mgenerate.core.operator.general;
 import org.junit.jupiter.api.Test;
 import uk.dioxic.mgenerate.common.Resolvable;
 import uk.dioxic.mgenerate.common.Wrapper;
-import uk.dioxic.mgenerate.core.operator.mutator.Concat;
-import uk.dioxic.mgenerate.core.operator.mutator.ConcatBuilder;
 import uk.dioxic.mgenerate.core.transformer.ReflectiveTransformerRegistry;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ArrayTest {
-
-    @Test
-    void resolve_SingleValues() {
-        List<Object> list = Arrays.asList("bob", "gertrude");
-        Concat concat = new ConcatBuilder(ReflectiveTransformerRegistry.getInstance())
-                .values(list)
-                .build();
-
-        assertThat(concat.resolveInternal()).containsExactlyElementsOf(list);
-    }
 
     @Test
     void resolve_StaticValues() {
